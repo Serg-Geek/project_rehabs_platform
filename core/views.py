ee09c38d59bf37fad7bc00f98f7f4406effb3ece
Serg-Geek <svgordienko@yandex.ru>
2025-04-08 15:32:08 +0400

    feat: add specialists section to homepage

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from facilities.models import RehabCenter, Clinic
+from staff.models import MedicalSpecialist
 
 # Create your views here.
 
@@ -8,7 +10,22 @@ class HomeView(TemplateView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Здесь можно добавить дополнительные данные для контекста
+        
+        # Получаем активные реабилитационные центры
+        context['rehab_centers'] = RehabCenter.objects.filter(
+            is_active=True
+        ).select_related('city', 'city__region')[:6]
+        
+        # Получаем активные клиники
+        context['clinics'] = Clinic.objects.filter(
+            is_active=True
+        ).select_related('city', 'city__region')[:6]
+        
+        # Получаем активных специалистов
+        context['specialists'] = MedicalSpecialist.objects.filter(
+            is_active=True
+        )[:6]
+        
         return context
 
 class ContactsView(TemplateView):
