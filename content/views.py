5362687f409855fe7c2f6dbf8795051942ec3f22
Serg-Geek <svgordienko@yandex.ru>
2025-04-20 18:47:26 +0400

    fix(banner): remove duplicate banner template and update styles
    - Remove duplicate banner.html template from content directory\n- Update BannerView to use index.html template\n- Modify HomeView to pass all active banners\n- Update index.html to use banner component via include\n- Remove banners.css link as styles are in style.css\n- Add conditional rendering for banner caption based on title/description

--- content/views.py
+++ content/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 from django.http import Http404
-from .models import StaticPage
+from .models import Banner, StaticPage
+from django.utils import timezone
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        today = timezone.now().date()
+        banners = Banner.objects.filter(
+            is_active=True,
+            start_date__lte=today,
+            end_date__gte=today
+        ).order_by('order')
+        context['banners'] = banners
+        return context
 
 class StaticPageView(DetailView):
     """Представление для отображения статической страницы"""
@@ -17,3 +32,20 @@ class StaticPageView(DetailView):
         if not obj.is_active:
             raise Http404("Страница не найдена или неактивна")
         return obj
+
+class BannerView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        today = timezone.now().date()
+        
+        # Получаем все активные баннеры
+        banners = Banner.objects.filter(
+            is_active=True,
+            start_date__lte=today,
+            end_date__gte=today
+        ).order_by('order')
+        
+        context['banners'] = banners
+        return context
