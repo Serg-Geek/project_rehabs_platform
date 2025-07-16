754e3c7282a97ee5d2c405cadc25e96486b1213d
Serg-Geek <svgordienko@yandex.ru>
2025-07-16 17:52:43 +0400

    feat(admin): add Russian verbose_name for all app configs (admin section names)

--- facilities/apps.py
+++ facilities/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class FacilitiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'facilities'
+    verbose_name = _('Учреждения')
--- recovery_stories/apps.py
+++ recovery_stories/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class RecoveryStoriesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'recovery_stories'
+    verbose_name = _('Истории выздоровления')
 
     def ready(self):
         import recovery_stories.signals
